2e8589229357615f8ec45232af0149d10a45b41b
Likun96 <biswajitbiswal96@hotmail.com>
2017-07-09 14:07:05 +0530

    Add files via upload

---
+++ biswa2.java
@@ -0,0 +1,16 @@
+import java.util.*;
+import java.lang.*;
+import java.io.*;
+ 
+/* Name of the class has to be "Main" only if the class is public. */
+class Ideone
+{
+	public static void main (String[] args) throws java.lang.Exception
+	{
+	int a=123;
+	if (a%2==0)
+	System.out.println("EVEN");
+	else
+	System.out.println("ODD");// your code goes here
+	}
+}
